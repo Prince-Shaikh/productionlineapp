79878f02b901a9f9d413aff887072f48b57c3393
Maestro <mzshaikh129@gmail.com>
2023-07-07 03:27:27 +0500

    Completed the Products Listing Screen

--- screens/ProductList.js
+++ screens/ProductList.js
@@ -1,44 +1,49 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { StyleSheet, Image } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
+
+const products = [
+  {
+    id: 1,
+    name: "M8  NX 55.3",
+    image: require("../assets/products/m8_55-3.png"),
+  },
+  {
+    id: 2,
+    name: "M8 NX 55.4",
+    image: require("../assets/products/m8_55-4.png"),
+  },
+  { id: 3, name: "PB 70", image: require("../assets/products/PB_70.jpg") },
+  { id: 4, name: "PB 70", image: require("../assets/products/PB_70.jpg") },
+  { id: 5, name: "PB 70", image: require("../assets/products/PB_70.jpg") },
+  { id: 6, name: "PB 70", image: require("../assets/products/PB_70.jpg") },
+];
 
 export default function ProductList() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.main}>
         <Text style={styles.text}>SELECT PRODUCT</Text>
-        <View style={styles.cardContainer}>
-          <View style={styles.card}>
-            <Image
-              style={styles.image}
-              source={require("../assets/Logo.png")}
-            />
-            <View style={styles.cardBottom}>
-              <Text style={styles.cardText}>Product Name</Text>
-            </View>
+        <ScrollView
+          style={styles.scroll}
+          contentContainerStyle={{
+            paddingBottom: 140,
+          }}
+        >
+          <View style={styles.cardContainer}>
+            {products.map((product) => {
+              return (
+                <View style={styles.card} key={product.id}>
+                  <Image style={styles.image} source={product.image} />
+                  <View style={styles.cardBottom}>
+                    <Text style={styles.cardText}>{product.name}</Text>
+                  </View>
+                </View>
+              );
+            })}
           </View>
-
-          <View style={styles.card}>
-            <Image
-              style={styles.image}
-              source={require("../assets/Logo.png")}
-            />
-            <View style={styles.cardBottom}>
-              <Text style={styles.cardText}>Product Name</Text>
-            </View>
-          </View>
-
-          <View style={styles.card}>
-            <Image
-              style={styles.image}
-              source={require("../assets/Logo.png")}
-            />
-            <View style={styles.cardBottom}>
-              <Text style={styles.cardText}>Product Name</Text>
-            </View>
-          </View>
-        </View>
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
@@ -50,6 +55,10 @@ const styles = StyleSheet.create({
     padding: 8,
     paddingBottom: 0,
   },
+  scroll: {
+    // flex: 1,
+    backgroundColor: "red",
+  },
   main: {
     flex: 1,
     alignItems: "center",
@@ -58,7 +67,9 @@ const styles = StyleSheet.create({
   },
   text: { fontSize: 32, fontWeight: "800", marginVertical: 24 },
   cardContainer: {
-    justifyContent: "center",
+    // flex: 1,
+    // flexGrow: 1,
+    // justifyContent: "center",
     flexDirection: "row",
     flexWrap: "wrap",
     width: "100%",
@@ -72,6 +83,8 @@ const styles = StyleSheet.create({
     padding: 8,
     justifyContent: "space-between",
     alignItems: "center",
+    minHeight: 250,
+    maxHeight: 450,
     width: "45%",
     height: "45%",
     backgroundColor: "white",
@@ -85,7 +98,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "80%",
     backgroundColor: "#CFCFCF",
-    resizeMode: "center",
+    resizeMode: "cover",
   },
   cardBottom: { alignItems: "center", justifyContent: "center", flex: 1 },
   cardText: { fontSize: 32, fontWeight: 600 },
